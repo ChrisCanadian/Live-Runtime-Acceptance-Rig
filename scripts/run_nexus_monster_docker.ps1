b717d5114bde412c4167ac2b9533078c7902b9b7
ChrisCanadian <chris.canadian1991@gmail.com>
2026-09-19 01:45:07 -0400

    fix(monster): pin SSR compatibility repair

--- scripts/run_nexus_monster_docker.ps1
+++ scripts/run_nexus_monster_docker.ps1
@@ -12,7 +12,7 @@ param(
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-$NDKA_SHA = "dff170c7c421d2b74369f0020c612b241cce9847"
+$NDKA_SHA = "aaae2cbf84da2d6b2a094175342f8367837c7185"
 $PRODUCTION_SHA = "2514a11366f8e7f345bb854c0cfaee8c7b40dddd"
 $V5_SHA = "48932a94a58f24f54b2fbe81c9d400ddb32f82ed"
 
